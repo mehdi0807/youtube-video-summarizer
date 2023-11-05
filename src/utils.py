c47b0ac683b90a8f27b85dd0aa0ae55cffb68845
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-05 14:28:51 +0100

    Changing summarizer function in utils.py
    Make the hf model bert-large-cnn by default

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 from langchain.document_loaders import YoutubeLoader
 import requests
 
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"  #The API URL to the `bert-large-cnn` model in hugging face
+
 def transcript_loader(url: str) -> str:
     """
     This function takes as argument the url of the video 
@@ -10,7 +12,7 @@ def transcript_loader(url: str) -> str:
     transcript = loader.load()
     return transcript[0].page_content
 
-def summarizer(text: str, api_url: str, headers:str) -> str:
+def summarizer(text: str, headers:str, api_url: str=API_URL) -> str:
     """
     This function takes as argument a text, an API url of the hugging face model (`bert-large-cnn` by default)
     and returns the summarized text
