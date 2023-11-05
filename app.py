10dbeb57fd0322f9e2e0e981163070b9ce75dedd
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-05 16:08:16 +0100

    Makinga slightly better front end

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 from src.utils import *
 
-st.title('😁🔗 Video Summarizer')
+# Some CSS styling (removing made with streamlit)
+css = """
+<style>
+.st-emotion-cache-h5rgaw  {
+    font-size: 0px;
+}"""
+st.markdown(css, unsafe_allow_html=True)
+
+# App
+st.title('🎬🔗 Video Summarizer')
 url = st.text_input('Plug in your video link here')
 
 if url:
     transcript = transcript_loader(url)
     output = summarizer(transcript)
-    st.write(output) 
+    st.markdown(output) 
