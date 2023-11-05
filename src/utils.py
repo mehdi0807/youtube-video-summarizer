cec113be92d2fa1ab540d60174847eb3ca71c077
Mehdi AMOR OUAHMED <mehdi.amor_ouahmed@g.enp.edu.dz>
2023-11-05 14:43:59 +0100

    Adding my Huggingface API key as default argument in the summarizer function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
 from langchain.document_loaders import YoutubeLoader
 import requests
+import os
+from dotenv import find_dotenv, load_dotenv
 
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"  #The API URL to the `bert-large-cnn` model in hugging face
+
+# Finding the Hugging face API from the .env file
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+HEADERS = os.getenv("headers")
 
 def transcript_loader(url: str) -> str:
     """
@@ -12,7 +19,7 @@ def transcript_loader(url: str) -> str:
     transcript = loader.load()
     return transcript[0].page_content
 
-def summarizer(text: str, headers:str, api_url: str=API_URL) -> str:
+def summarizer(text: str, api_url: str=API_URL, headers:str=HEADERS) -> str:
     """
     This function takes as argument a text, an API url of the hugging face model (`bert-large-cnn` by default)
     and returns the summarized text
